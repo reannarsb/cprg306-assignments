3fbcb83292c4fea2be0fcb05c07855824f19ed38
reannarsb <reanna.rsb@gmail.com>
2024-05-17 12:17:24 -0600

    rest of assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return(
+        <main>
+            <h1>Student Info</h1>
+            <p>Name: Reanna Bari</p>
+            <Link href="https://github.com/reannarsb">Github Profile</Link>
+
+        </main>
+    );
+}
